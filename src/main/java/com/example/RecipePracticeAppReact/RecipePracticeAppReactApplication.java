957f6a031c5b229c10ae720060c256c6cce4dcf1
Jillian Fusco <fuscojillian@gmail.com>
2024-11-20 13:26:58 -0500

    initial commit

---
+++ src/main/java/com/example/RecipePracticeAppReact/RecipePracticeAppReactApplication.java
@@ -0,0 +1,13 @@
+package com.example.RecipePracticeAppReact;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecipePracticeAppReactApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecipePracticeAppReactApplication.class, args);
+	}
+
+}
